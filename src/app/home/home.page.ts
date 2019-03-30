e98b95f94db655f1528e113e519958be4b515d3d
Isabella Borges <isasousaah@gmail.com>
2019-03-30 20:04:08 -0300

    Organizando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,48 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpErrorResponse} from '@angular/common/http';
 import { Carro } from '../modelos/Carro';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
-import { LoadingController, AlertController } from '@ionic/angular';
-import { Button } from 'protractor';
-import { async } from 'q';
+import { LoadingController, AlertController, NavController } from '@ionic/angular';
+import { CarrosService } from '../providers/carros.service';
+import { NavigationExtras } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-
+export class HomePage implements OnInit {
   public carros: Carro[];
 
-  constructor (public http:HttpClient,
-    private loadingCtrl:LoadingController,
-    private alertCtrl:AlertController){
-
+  constructor(private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController,
+              private carrosService: CarrosService,
+              private navCtrl: NavController){
   }
 
   async ngOnInit() {
     const loading = await this.loadingCtrl.create({
-      message: "Aguarde enquanto os carros são carregados."
+      message: 'Aguarde enquanto os carros são carregados...'
     });
+
     await loading.present();
 
-    this.http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')   
+    this.carrosService.lista()
     .subscribe(
-       (carros)=>{
-         this.carros=carros;
-       },
-       async (err: HttpErrorResponse)=>{
-          console.log("Erro" + err.status);
-          const al = await this.alertCtrl.create({
-            header: "Erro",
-            message: "Erro ao listar os carros.",
-            buttons: [{text: "OK"}]
-          });
+      (carros)=>{
+        this.carros = carros;
+      },
+      async (err: HttpErrorResponse)=>{
+        console.log('Deu erro ' + err.status);
+        const al = await this.alertCtrl.create({
+          header:'Erro!',
+          message: 'Erro ao listar carros',
+          buttons: [{text: 'OK'}]
+        });
+
         await al.present();
-       }
+      }
     ).add(
       ()=>{
         loading.dismiss();
       }
     )
+  } 
+
+  selecionaCarro(carro: Carro){
+    console.log("Carro selecionado: " + carro.nome);
+
+    let extras: NavigationExtras = {
+      queryParams:{
+        carroSelecionado: JSON.stringify(carro)
+      }
+    };
+
+    this.navCtrl.navigateForward(['escolha'], extras);
   }
+
 }
