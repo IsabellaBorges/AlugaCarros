01c912c52f9a4fa59595cb9dfb92f1cf54fdbaeb
Isabella Borges <isasousaah@gmail.com>
2019-03-30 20:07:21 -0300

    organizando

---
+++ src/app/escolha/escolha.page.ts
@@ -0,0 +1,29 @@
+import { NavController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Carro } from '../modelos/Carro';
+
+
+@Component({
+  selector: 'app-escolha',
+  templateUrl: './escolha.page.html',
+  styleUrls: ['./escolha.page.scss'],
+})
+export class EscolhaPage implements OnInit {
+
+  constructor(private navCtrl: NavController,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+this.activatedRoute.queryParams
+.subscribe(params => {
+  let carro = <Carro>JSON.parse(params['carroSelecionado']);
+  console.log('O carro que chegou na pagina de escolha é: ' + carro.nome);
+});
+  }
+
+  voltar() {
+    this.navCtrl.back();
+  }
+
+}
