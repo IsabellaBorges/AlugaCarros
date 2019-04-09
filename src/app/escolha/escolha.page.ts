c995d7c518ab6e659c7ac2a570d4a39243afbf09
Isabella Borges <isasousaah@gmail.com>
2019-04-08 23:55:25 -0300

    Aula 08/04

--- src/app/escolha/escolha.page.ts
+++ src/app/escolha/escolha.page.ts
@@ -9,16 +9,16 @@ import { Carro } from '../modelos/Carro';
   styleUrls: ['./escolha.page.scss'],
 })
 export class EscolhaPage implements OnInit {
-
+  private carro:Carro
   constructor(private navCtrl: NavController,
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.activatedRoute.queryParams
       .subscribe(params =>{
-        let carro = <Carro>JSON.parse(params["carroSelecionado"]);
+        this.carro = <Carro>JSON.parse(params["carroSelecionado"]);
 
-        console.log("O carro que chegou na pagina de escolha é: " + carro.nome);
+        console.log("O carro que chegou na pagina de escolha é: " + this.carro.nome);
       });
   }
 
